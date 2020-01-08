de906fd5b5271f4bc3c6012cf420f14385960591
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2020-01-08 22:08:37 +0600

    Basic Structure

--- src/components/top_banner/top_banner.js
+++ src/components/top_banner/top_banner.js
@@ -1,13 +1,21 @@
 import React, {Component, Fragment} from 'react';
-import {Container,Row} from "react-bootstrap";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import '../../asset/css/custom.css'
 class TopBanner extends Component {
     render() {
         return (
             <Fragment>
-                <Container className="topFixedBanner" >
+                <Container fluid={true} className="topFixedBanner p-0" >
                     <div className="topBannerOverlay" >
-
+                        <Container className="topContent" >
+                            <Row>
+                                <Col>
+                                    <h1>Glitch Studios</h1>
+                                    <h4>A hub of Software Engineers</h4>
+                                    <Button variant="primary">info</Button>
+                                </Col>
+                            </Row>
+                        </Container>
                     </div>
                 </Container>
             </Fragment>
