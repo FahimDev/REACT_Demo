597be2a0ca5c55b3f4d2a9e67e9a0a73b2d1c9c4
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2020-01-09 19:25:31 +0600

    NavBar Logo added. [logo swap]

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,21 +1,26 @@
 import React, {Component, Fragment} from 'react';
 import {Nav, Navbar} from "react-bootstrap";
 import '../../asset/css/custom.css'
+import navBarLogo0 from '../../asset/img/navLogo.png'
+import navBarLogo1 from'../../asset/img/navLogo0.png'
 
 class NavBar extends Component {
     constructor() {
         super();
         this.state={
-            navBar:"statNav"
+            navBar:"statNav",
+            navLogo0:[navBarLogo0]
         }
 
     }
     onScroll=()=>{
         if(window.scrollY>100){
             this.setState({navBar:'dyneNav'})
+            this.setState({navLogo0:[navBarLogo1]})
         }
         else if(window.scrollY<100){
             this.setState({navBar:'statNav'})
+            this.setState({navLogo0:[navBarLogo0]})
         }
     }
 
@@ -27,7 +32,7 @@ class NavBar extends Component {
         return (
             <Fragment>
                 <Navbar fixed="top" bg="dark" variant="dark">
-                    <Navbar.Brand className={this.state.navBar}  href="#home">Glitch</Navbar.Brand>
+                    <Navbar.Brand className={this.state.navBar}  href="#home"> <img src={this.state.navLogo0} height="30rem" />   Glitch</Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Services</Nav.Link>
