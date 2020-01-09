9d45254b218da3dfc4260782b86064c0e96404f4
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2020-01-09 16:47:33 +0600

    Nav Scrolling (use of constructor & funcion)

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,12 +1,33 @@
 import React, {Component, Fragment} from 'react';
 import {Nav, Navbar} from "react-bootstrap";
+import '../../asset/css/custom.css'
 
 class NavBar extends Component {
+    constructor() {
+        super();
+        this.state={
+            navBar:"statNav"
+        }
+
+    }
+    onScroll=()=>{
+        if(window.scrollY>100){
+            this.setState({navBar:'dyneNav'})
+        }
+        else if(window.scrollY<100){
+            this.setState({navBar:'statNav'})
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll',this.onScroll)
+    }
+
     render() {
         return (
             <Fragment>
                 <Navbar fixed="top" bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Glitch</Navbar.Brand>
+                    <Navbar.Brand className={this.state.navBar}  href="#home">Glitch</Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Services</Nav.Link>
